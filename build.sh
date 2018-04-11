d2b747c4fa1e830c231530bb21884a8edf94e822
boyie <5917613@qqcom>
2018-04-11 15:28:40 +0800

    ADAS

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ export ARCH=arm
 export CROSS_COMPILE=/opt/Embedsky/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/tq-linaro-toolchain/bin/arm-none-linux-gnueabi-
 make distclean
 make mx6qe9_config
-make -j8
+#make -j8
+
+
 
